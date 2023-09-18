d894d7ba9bd671bdfd336cdbe45aa8cd03a44225
John Steban Lopez <104475729+JohnGolgota@users.noreply.github.com>
2023-09-18 09:15:55 -0500

    Los desastres naturales son graciosos

--- bin/Custom_Funciones.ps1
+++ bin/Custom_Funciones.ps1
@@ -6,8 +6,19 @@ $RepoHashPath = $MyPaths[0]
 $RepoCLIPath = $MyPaths[1]
 
 Set-Variable -Name "splitedEnv" -Value $env:Path.Split(";")
+function Remove-ToRepoCLI {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$RepoToRemove
+    )
+    try {
 
-function Add-RepoToHashtable {
+    }
+    catch {
+        Write-Host "Error: $($_.Exception.Message)"
+    }
+}
+function Add-ToRepoCLI {
     param (
         [Parameter(Mandatory = $true)]
         [string]$NewRepoName,
@@ -17,25 +28,33 @@ function Add-RepoToHashtable {
 
         [bool]$Force = $false
     )
-    $RepoHash = @{}
-    if ($Force -eq $false) {
-        . $RepoHashPath
+    try {
+        
+        $RepoHash = @{}
+        if ($Force -eq $false) {
+            . $RepoHashPath
+        }
+        $RepoNames = @()
+        # TODO validate if repo exists
+        $RepoHash.Add($NewRepoName, $NewRepoPath)
+
+        Set-Content -Path $RepoHashPath -Value '$RepoHash = @{'
+        foreach ($Repo in $RepoHash.GetEnumerator()) {
+            Add-Content -Path $RepoHashPath -value "`"$($Repo.Key)`"=`"$($Repo.Value)`""
+            # array from hashtable keys
+            $RepoNames += $Repo.Key
+        }
+        Add-Content -Path $RepoHashPath -value "}"
+    
+        $RepoNamesJoin = $RepoNames -join "','"
+        Add-Content -Path $RepoHashPath -value $('$RepoNames = @(' + "'$RepoNamesJoin'" + ')')
+
+        Build-RepoCLI
     }
-    $RepoNames = @()
-    # TODO validate if repo exists
-    $RepoHash.Add($NewRepoName, $NewRepoPath)
-
-    Set-Content -Path $RepoHashPath -Value '$RepoHash = @{'
-    foreach ($Repo in $RepoHash.GetEnumerator()) {
-        Add-Content -Path $RepoHashPath -value "`"$($Repo.Key)`"=`"$($Repo.Value)`""
-        # array from hashtable keys
-        $RepoNames += $Repo.Key
+    catch {
+        Write-Host "Error: $($_.Exception.Message)"
     }
-    Add-Content -Path $RepoHashPath -value "}"
-
-    $RepoNamesJoin = $RepoNames -join "','"
-    Add-Content -Path $RepoHashPath -value $('$RepoNames = @('+"'$RepoNamesJoin'"+')')
-
+    
 }
 
 function Build-RepoCLI {
@@ -43,27 +62,33 @@ function Build-RepoCLI {
     $RepoNames = @()
     . $RepoHashPath
     $RepoNames = $RepoNames -join "','"
-    Set-Content -Path $RepoCLIPath -Value "param ("
-    Add-Content -Path $RepoCLIPath -Value "[ValidateSet('$RepoNames')]" 
-    Add-Content -Path $RepoCLIPath -Value '[string]$Repo = "NoRepoProvided"'
-    Add-Content -Path $RepoCLIPath -Value ")"
-    Add-Content -Path $RepoCLIPath -Value "try {"
-    Add-Content -Path $RepoCLIPath -Value 'Write-Host "Repo: $Repo"'
-    Add-Content -Path $RepoCLIPath -Value '$repos = @{'
+    Set-Content -Path $RepoCLIPath -Value @"
+    param (
+        [ValidateSet('$RepoNames')]
+        [string]`$Repo = "NoRepoProvided"
+    )
+    try {
+        Write-Host "Repo: `$Repo"
+
+        `$repos = @{
+"@
     foreach ($Repo in $RepoHash.GetEnumerator()) {
         Add-Content -Path $RepoCLIPath -Value "`"$($Repo.Key)`" = `"$($Repo.Value)`""
     }
-    Add-Content -Path $RepoCLIPath -Value "}"
-    Add-Content -Path $RepoCLIPath -Value 'if ($repos.ContainsKey($Repo)) {'
-    Add-Content -Path $RepoCLIPath -Value 'code-insiders $repos[$Repo]'
-    Add-Content -Path $RepoCLIPath -Value "} else {"
-    Add-Content -Path $RepoCLIPath -Value 'code-insiders'
-    Add-Content -Path $RepoCLIPath -Value "}"
-    Add-Content -Path $RepoCLIPath -Value "}"
-    Add-Content -Path $RepoCLIPath -Value "catch {"
-    Add-Content -Path $RepoCLIPath -Value 'Write-Host "Error: $($_.Exception.Message)"'
-    Add-Content -Path $RepoCLIPath -Value "}"
+    Add-Content -Path $RepoCLIPath -Value @"
+        }
+        if (`$repos.ContainsKey(`$Repo)) {
+            code-insiders `$repos[`$Repo]
+        } else {
+            code-insiders
+        }
+    }
+    catch {
+        Write-Host "Error: `$(`$_.Exception.Message)"
+    }
+"@
 }
+
 function Comprobar {
     $Nombre = Read-Host "¿Cómo te llamas?"
     Write-Host "Hola $Nombre"
