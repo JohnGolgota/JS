30d21923334f8a5ffb7d05297a51aabaa585d171
JohnGolgota <js684new@gmail.com>
2024-01-07 14:28:38 -0500

    config home wsl

--- .config/config.bash.sh
+++ .config/config.bash.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
 setup_git() {
-    git config --global user.name "JohnGolgota"
-    git config --global user.email "js684new@gmail.com"
+	git config --global user.name "JohnGolgota"
+	git config --global user.email "js684new@gmail.com"
 }
 
 # nodejs
 setup_nodejs() {
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
-  nvm install --lts
-  # install pnpm
-  corepack enable
-  corepack prepare pnpm@latest --activate
+	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+	nvm install --lts
+	# install pnpm
+	corepack enable
+	corepack prepare pnpm@latest --activate
 }
 setup_nvim() {
-  git clone https://github.com/github/copilot.vim.git \
-    ~/.config/nvim/pack/github/start/copilot.vim
-  # :Copilot setup
+	git clone https://github.com/github/copilot.vim.git \
+		~/.config/nvim/pack/github/start/copilot.vim
+	# :Copilot setup
 }
 use_vscode() {
-  code-insiders.cmd
-  code.cmd
+	code-insiders.cmd
+	code.cmd
 }
 setup_bashpersonal() {
-  touch ~/.bash_aliases
-  echo "alias c=code-insiders" >> ~/.bash_aliases
-  echo "alias x=nvim" >> ~/.bash_aliases
-  echo "alias z=tmux" >> ~/.bash_aliases
+	touch ~/.bash_aliases
+	echo "alias c=code-insiders" >> ~/.bash_aliases
+	echo "alias x=nvim" >> ~/.bash_aliases
+	echo "alias z=tmux" >> ~/.bash_aliases
 }
 
 sudo apt-get update
