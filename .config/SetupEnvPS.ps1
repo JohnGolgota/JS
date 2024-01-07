e624121b96940f8dd50545884939915e424c9471
JohnGolgota <js684new@gmail.com>
2024-01-06 23:51:01 -0500

    Yo se como duele cuando se ama a quien no se debe

--- .config/SetupEnvPS.ps1
+++ .config/SetupEnvPS.ps1
@@ -1,33 +1,40 @@
+param (
+	# Ruta al archivo de entorno
+	[Parameter(Mandatory = $true)]
+	[string]$PathEnvFile
+)
 try {
-	# environment file
-	$PathEnvFile = ".env"
-
-	# Content
+	# Contenido del archivo de entorno
 	$ContentEnv = Get-Content -Path $PathEnvFile
 
-	# Iterate
+	# Iterar sobre cada línea del archivo de entorno
 	$ContentEnv | ForEach-Object {
-		# Split
+		# Separar la línea en nombre y valor usando el signo igual
 		$Name, $Value = $_.Split("=")
 
-		# Trim
+		# Limpiar el valor eliminando espacios alrededor
 		$Value = $Value.Trim()
 
-		# Debug
+		# Debug (descomentar para depuración)
 		# Write-Host "Name: $($Name) Value: $($Value)"
+
 		if ($Value -eq "") {
 			Write-Host "Skip: $Name Value: $Value"
 			continue
 		}
+
+		# Establecer la variable de entorno de forma persistente a nivel de usuario
 		[System.Environment]::SetEnvironmentVariable($Name, $Value, "User")
 		Write-Host "Set: $Name Value: $Value"
 	}
+
 	if ($null -ne $env:JS -and $env:JS -ne "") {
+		# Agregar la ruta al directorio JS al Path
 		[System.Environment]::SetEnvironmentVariable("Path", "$env:JS/.bin;$env:Path", "User")
 		Write-Host "Done"
 		exit 0
 	}
-	else {
+ else {
 		Write-Host "Error: JS environment variable not found"
 		exit 1
 	}
