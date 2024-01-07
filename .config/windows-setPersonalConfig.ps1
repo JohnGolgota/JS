5184e3dee42f09a0b50838cab6b409a55ca5c5e7
JohnGolgota <js684new@gmail.com>
2024-01-07 14:18:22 -0500

    joden mucho

--- .config/windows-setPersonalConfig.ps1
+++ .config/windows-setPersonalConfig.ps1
@@ -41,83 +41,17 @@ function SetSecond {
 	corepack enable `
 		corepack prepare pnpm@latest --activate
 }
-wsl {
+function setupwsl {
+	function setupDebian {
+		$lowercaseJS = $env:JS.ToLower()
+		$caracterquejode = ":"
+		$caracterquepongo = "/"
+		$lowercaseJS = $lowercaseJS.Replace($caracterquejode, $caracterquepongo)
+		bash "/mnt/$lowercaseJS/.config/config.bash.sh"
+	}
 	# WSL2 https://docs.microsoft.com/en-us/windows/wsl/install-win10
 	wsl --install -d Debian
 	wsl --set-default-version 2
-	# Dependecies wsl
-	sudo apt update
-	sudo apt upgrade -y
-	# Dependecies avaliable with apt
-	sudo apt install -y curl wget git zsh zip unzip gzip tar
-	# tools
-	sudo apt install -y tmux
-	# Dependecies neovim
-	git config --global user.name "JohnGolgota"
-	git config --global user.email "js684new@gmail.com"
-	node js {
-		nvm & pnpm {
-			# Dependecies node https://github.com/nvm-sh/nvm#installing-and-updating
-			curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
-			nvm install --lts
-		}
-		pnpm {
-			# https://pnpm.io/es/installation
-			curl -fsSL https://get.pnpm.io/install.sh | sh -
-		}
-	}
-	neovim {
-		# https://github.com/neovim/neovim/wiki/Installing-Neovim#linux
-		curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-		chmod u+x nvim.appimage
-		./nvim.appimage
-		# If the ./nvim.appimage command fails, try:
-		err {
-			./nvim.appimage --appimage-extract
-			./squashfs-root/AppRun --version
 
-			# Optional: exposing nvim globally.
-			sudo mv squashfs-root /
-			sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
-			nvim
-		}
-		copilot {
-			# nvim Copilot https://github.com/github/copilot.vim
-			git clone https://github.com/github/copilot.vim.git \
-			~/.config/nvim/pack/github/start/copilot.vim
-			{
-				:Copilot setup
-			}
-		}
-		plugvim {
-			# https://github.com/junegunn/vim-plug
-			Invoke-WebRequest -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
-				New-Item "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
-		}
-	}
-	code & code-insiders {
-		# Configs vscode https://code.visualstudio.com/docs/remote/wsl
-		code-insiders.cmd
-		code.cmd
-	}
-	redis {
-		# https://redis.io/docs/getting-started/installation/install-redis-on-windows/
-		curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
-
-		echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
-
-		sudo apt-get update
-		sudo apt-get install redis
-	}
-	rust {
-		# https://www.rust-lang.org/tools/install
-		curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-	}
-	profilings {
-		nvim ~/.bash_aliases | nvim ~/.zshrc | nvim ~/.bashrc
-		{
-			alias x = nvim
-			alias c = code-insiders
-		}
-	}
+	setupDebian
 }
