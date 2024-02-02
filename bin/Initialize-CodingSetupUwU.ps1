590249979d38b7ec77c59a24d4c302aaf2d56d6b
John Steban Lopez <104475729+JohnGolgota@users.noreply.github.com>
2024-02-02 09:48:15 -0500

    a

---
+++ bin/Initialize-CodingSetupUwU.ps1
@@ -0,0 +1,6 @@
+codium.cmd && code-insiders.cmd
+
+. "$Env:APPDATA\Microsoft\Windows\Start Menu\Programs\DBeaver Community\DBeaver.lnk"
+. "$Env:APPDATA\Microsoft\Windows\Start Menu\Programs\Kong\Insomnia.lnk"
+
+
