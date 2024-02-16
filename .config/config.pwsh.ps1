bf1655837a203255c26e9d7853bbb2944c5df46c
JohnGolgota <js684new@gmail.com>
2024-02-16 03:23:01 -0500

    e

--- .config/config.pwsh.ps1
+++ .config/config.pwsh.ps1
@@ -1,4 +1,16 @@
+param (
+	[Parameter]
+	[string]$JS = ""
+)
 Write-Host "Configuring powershell..."
+
 New-Item $PROFILE -Type File -Force
+
+if ($JS -eq "") {
+	$JS = Read-Host "Enter the path to the JS folder"
+	Add-Content -Path $PROFILE -Value "`$env:JS = `"$JS`""
+}
+
+Add-Content -Path $PROFILE -Value "Set-PSReadLineOption -EditMode Windows"
 Add-Content -Path $PROFILE -Value ". Custom_Funciones.ps1"
 Add-Content -Path $PROFILE -Value "Set-CustomMain"
