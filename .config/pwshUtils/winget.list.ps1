c67e45c4d687b09d5f31d5c2e30985f2e11472a3
JohnGolgota <js684new@gmail.com>
2024-05-05 06:14:10 -1100

    feat: no entiendo sqlite

--- .config/pwshUtils/winget.list.ps1
+++ .config/pwshUtils/winget.list.ps1
@@ -10,16 +10,20 @@ $winget_list.Sources.Packages | ForEach-Object {
 	$query = "SELECT * FROM Applications WHERE id LIKE '%$($_.PackageIdentifier)%' AND source = 1"
 	$AplicationsList = Invoke-SqliteQuery -DataSource ".\.data\main.db" -Query $query
 	if ($AplicationsList) {
-		write-host "if $($_.PackageIdentifier)"
+		# write-host "if $($_.PackageIdentifier)"
+		$query = "UPDATE Applications SET installed = 1 WHERE id LIKE '%$($_.PackageIdentifier)%' AND source = 1 AND i_use_that = 1"
+		Invoke-SqliteQuery -DataSource ".\.data\main.db" -Query $query
 		return
 	}
-	$query = "INSERT INTO Applications (id, name, source, i_use_that) VALUES ('$($_.PackageIdentifier)', '$($_.PackageIdentifier)', 1, 2)"
+	$query = "INSERT INTO Applications (id, name, source, i_use_that, installed) VALUES ('$($_.PackageIdentifier)', '$($_.PackageIdentifier)', 1, 2, 1)"
 	Invoke-SqliteQuery -DataSource ".\.data\main.db" -Query $query
 }
 
-$query = "SELECT * FROM Applications WHERE install = 1 AND 'source' = 1"
-
-# $AplicationsList | ForEach-Object {
-# 	Write-Host "winget install --id $_ -e --source winget"
-# 	# winget install --id $_ -e --source winget
-# }
+$query = 'SELECT * FROM Applications WHERE installed = 0 AND "source" = 1 AND i_use_that = 1'
+$AplicationsList = Invoke-SqliteQuery -DataSource ".\.data\main.db" -Query $query
+if ($AplicationsList) {
+	$AplicationsList | ForEach-Object {
+		Write-Host "winget install --id $_ -e --source winget"
+		# winget install --id $_ -e --source winget
+	}
+}
