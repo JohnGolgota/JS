b4ab82ba45af29a401e4f287cb8d61590eeee8c6
John Steban Lopez <104475729+JohnGolgota@users.noreply.github.com>
2024-05-24 20:35:53 +0200

    Debi abstraer esa monda

--- .config/pwshUtils/winget.list.ps1
+++ .config/pwshUtils/winget.list.ps1
@@ -12,19 +12,19 @@ if (-not (Test-Path $current_winget_path)) {
 $winget_list = Get-Content $current_winget_path | ConvertFrom-Json
 $winget_list.Sources.Packages | ForEach-Object {
 	# $AplicationsList = $applicationsRepository.GetByParam(@{id = "%$($_.PackageIdentifier)%"}, $true)
-	$query = "SELECT * FROM Applications WHERE id LIKE '%$($_.PackageIdentifier)%' AND source = 1"
+	$query = "SELECT * FROM Applications WHERE id LIKE '%$($_.PackageIdentifier)%' AND attr_source = 1"
 	$AplicationsList = Invoke-SqliteQuery -DataSource ".\.data\main.db" -Query $query
 	if ($AplicationsList) {
 		# write-host "if $($_.PackageIdentifier)"
-		$query = "UPDATE Applications SET installed = 1 WHERE id LIKE '%$($_.PackageIdentifier)%' AND source = 1 AND i_use_that = 1"
+		$query = "UPDATE Applications SET installed = 1 WHERE id LIKE '%$($_.PackageIdentifier)%' AND attr_source = 1 AND i_use_that = 1"
 		Invoke-SqliteQuery -DataSource ".\.data\main.db" -Query $query
 		return
 	}
-	$query = "INSERT INTO Applications (id, name, source, i_use_that, installed) VALUES ('$($_.PackageIdentifier)', '$($_.PackageIdentifier)', 1, 2, 1)"
+	$query = "INSERT INTO Applications (id, name, attr_source, i_use_that, installed) VALUES ('$($_.PackageIdentifier)', '$($_.PackageIdentifier)', 1, 2, 1)"
 	Invoke-SqliteQuery -DataSource ".\.data\main.db" -Query $query
 }
 
-$query = 'SELECT * FROM Applications WHERE installed = 0 AND "source" = 1 AND i_use_that = 1'
+$query = 'SELECT * FROM Applications WHERE installed = 0 AND attr_source = 1 AND i_use_that = 1'
 $AplicationsList = Invoke-SqliteQuery -DataSource ".\.data\main.db" -Query $query
 if ($AplicationsList) {
 	$AplicationsList | ForEach-Object {
