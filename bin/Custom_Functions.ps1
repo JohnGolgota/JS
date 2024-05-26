e7b422b2f04ea06c91c25c2fc01ab58c99b92124
JohnGolgota <104475729+JohnGolgota@users.noreply.github.com>
2024-05-26 18:20:40 -0500

    esto no es el final

--- bin/Custom_Functions.ps1
+++ bin/Custom_Functions.ps1
@@ -1,28 +1,34 @@
-function Set-CustomMain {
-    try {
+function Set-CustomMain
+{
+    try
+    {
         . $Env:JS\bin\pwshModuls\Set-CustomAlias.ps1
         . $Env:JS\bin\RepoCli-Utils.ps1
         . $Env:JS\.config\profile.pwsh.ps1
-    }
-    catch {
+        . $PROFILE
+    } catch
+    {
         Write-Host "Error: $($_.Exception.Message)"
     }
 }
 
-function Get-CodeWorkspace {
+function Get-CodeWorkspace
+{
     $folderInfo = Get-Item .
     $folderName = $folderInfo.Name
     $CodeWorkspace = Get-ChildItem -Path . -Filter *.code-workspace -Recurse -ErrorAction SilentlyContinue
-    if ($null -eq $CodeWorkspace) {
+    if ($null -eq $CodeWorkspace)
+    {
         Write-Host "No se encontró ningún archivo .code-workspace"
         Copy-Item -Path "$env:JS\copy\example.code-workspace" -Destination "./$folderName.code-workspace"
-    }
-    else {
+    } else
+    {
         Write-Host "Se encontró el archivo .code-workspace"
     }
 }
 
-function Set-TempEnvFromFile {
+function Set-TempEnvFromFile
+{
     param (
         [Parameter(Mandatory = $true)]
         [string]$EnvFile
@@ -33,9 +39,18 @@ function Set-TempEnvFromFile {
     }
 }
 
-function AdivinarNombre {
+function Hola
+{
     Write-Host "Voy a adivinar tu nombre"
     $Nombre = Read-Host "¿Cómo te llamas?"
     Write-Host "Tu nombre es $Nombre"
     curl ascii.live/donut
+}
+
+function Update-JS
+{
+    $currentDir = Get-Location
+    Set-Location $Env:JS
+    git pull
+    Set-Location $currentDir
 }
