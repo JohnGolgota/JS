b10a7d1ac6af9028a132cc41db272a5de8a1368b
JohnGolgota <104475729+JohnGolgota@users.noreply.github.com>
2024-06-19 09:44:23 -0500

    feat: mas variables en mac

--- .config/profile.pwsh.ps1
+++ .config/profile.pwsh.ps1
@@ -1,6 +1,13 @@
-function Set-NoWindowsEnvs {
+function Set-NoWindowsEnvs
+{
+	$Paths = @(
+		"${HOME}/.local/share/bob/nvim-bin",
+		"$Env:JS/bin"
+	)
+	$NewPath = $Paths -join ':'
+
 	Add-Content -Path $PROFILE -Value "`n`$Env:JS = `"$Env:HOME/JS`""
-	Add-Content -Path $PROFILE -Value "`$Env:PATH = `"$Env:JS/bin:`$Env:PATH`""
+	Add-Content -Path $PROFILE -Value "`$Env:PATH = `"${NewPath}:`$Env:PATH`""
 	Add-Content -Path $PROFILE -Value "`$Env:MY_PATHS = `"$Env:JS/private_h.ps1;$Env:JS/bin/repo.ps1`""
 
 	Add-Content -Path $PROFILE -Value "`n. $Env:JS/bin/Custom_Functions.ps1"
@@ -8,37 +15,56 @@ function Set-NoWindowsEnvs {
 	Add-Content -Path $PROFILE -Value ". $Env:JS/bin/PS_Alias.ps1"
 }
 
-function Set-WindowsEnvs {
+function Set-WindowsEnvs
+{
 	Add-Content -Path $PROFILE -Value ". $Env:JS\bin\Custom_Functions.ps1"
 	Add-Content -Path $PROFILE -Value ". $Env:JS\bin\RepoCli-Utils.ps1"
 	Add-Content -Path $PROFILE -Value ". $Env:JS\bin\PS_Alias.ps1"
 }
 
 Write-Host "Configuring powershell..."
-if (-not (Test-Path $PROFILE)) {
-    New-Item $PROFILE -Type File -Force
+if (-not (Test-Path $PROFILE))
+{
+	New-Item $PROFILE -Type File -Force
 }
 
 Set-Content -Path $PROFILE -Value ""
 
 Add-Content -Path $PROFILE -Value "Set-PSReadLineOption -EditMode Windows"
 
-if ($IsLinux) {
-	Add-Content -Path $PROFILE -Value "`n`$Env:PATH = `"${HOME}/.cargo/bin:${HOME}/.local/share/bob/nvim-bin:`$Env:PATH`""
+if ($IsLinux)
+{
+	Add-Content -Path $PROFILE -Value "`n`$Env:PATH = `"${HOME}/.cargo/bin:`$Env:PATH`""
 	Set-NoWindowsEnvs
 }
-if ($IsMacOS) {
-	Add-Content -Path $PROFILE -Value "`n`$Env:PATH = `"/opt/homebrew/opt/openjdk/bin:/opt/homebrew/bin:/opt/homebrew/sbin:/Applications/Docker.app/Contents/Resources/bin:`$Env:PATH`""
+if ($IsMacOS)
+{
+	$Paths = @(
+		"/opt/homebrew/opt/openjdk/bin",
+		"/opt/homebrew/bin",
+		"/opt/homebrew/sbin",
+		"/Applications/Docker.app/Contents/Resources/bin",
+		"${HOME}/.local/share/pnpm",
+		"${HOME}/.bun/bin",
+		"${HOME}/.nix-profile/bin",
+		"/nix/var/nix/profiles/default/bin",
+		"${HOME}/asd/bin"
+	)
+	$NewPath = $Paths -join ':'
+	Add-Content -Path $PROFILE -Value "`n`$Env:PATH = `"${NewPath}:`$Env:PATH`""
 	Set-NoWindowsEnvs
 }
-if ($IsWindows) {
+if ($IsWindows)
+{
 	Set-WindowsEnvs
 }
 
-if (Get-Command "fnm" -ErrorAction SilentlyContinue) {
+if (Get-Command "fnm" -ErrorAction SilentlyContinue)
+{
 	Add-Content -Path $PROFILE -Value "`nfnm env --use-on-cd | Out-String | Invoke-Expression"
 }
 
-if (Get-Command "starship" -ErrorAction SilentlyContinue) {
+if (Get-Command "starship" -ErrorAction SilentlyContinue)
+{
 	Add-Content -Path $PROFILE -Value "`nInvoke-Expression (&starship init powershell)"
 }
